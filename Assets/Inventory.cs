8bdda70da3cfec0ecbed86f94fd81c0e68a512e2
Allan Drake Paladin Dulay <40012648+allandrakeprojects@users.noreply.github.com>
2019-07-11 11:04:57 +0800

    Before update of Drag and Drop

---
+++ Assets/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour, IHasChanged
+{
+    [SerializeField] Transform slots;
+    [SerializeField] Text inventoryText;
+
+    private void Start()
+    {
+        HasChanged();
+    }
+
+    public void HasChanged()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append(" - ");
+        foreach (Transform slotTransform in slots)
+        {
+            GameObject item = slotTransform.GetComponent<Slots>().item;
+            if (item)
+            {
+                builder.Append(item.name);
+                builder.Append(" - ");
+            }
+        }
+
+        inventoryText.text = builder.ToString();
+    }
+}
+
+
+namespace UnityEngine.EventSystems
+{
+    public interface IHasChanged : IEventSystemHandler
+    {
+        void HasChanged();
+    }
+}
---
+++ Assets/Slots.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slots : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            Debug.Log("dasdas");
+            DragHandeler.itemBeingDragged.transform.SetParent(transform);
+            ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
+        }
+    }
+}
